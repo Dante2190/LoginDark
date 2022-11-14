a378ab125e367b02452e9e0fdd29cf69813dcbb9
Dante2190 <denzel2190@gmail.com>
2022-11-13 21:20:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/logindark/SplashscreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.logindark
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashscreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
